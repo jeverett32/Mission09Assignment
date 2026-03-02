5f9f928d9296b23ae213d89a9e84055fe22450c4
John Everett <everjohn@byu.edu>
2026-03-02 08:59:52 -0700

    changed spacing

--- frontend/src/components/Heading.tsx
+++ frontend/src/components/Heading.tsx
@@ -1,15 +1,15 @@
 type HeadingProps = {
-  title: string
-  subtitle: string
+    title: string
+    subtitle: string
 }
 
 function Heading({ title, subtitle }: HeadingProps) {
-  return (
-    <header className="heading">
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
-    </header>
-  )
+    return (
+        <header className="heading">
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+        </header>
+    )
 }
 
 export default Heading
--- frontend/src/components/TeamList.tsx
+++ frontend/src/components/TeamList.tsx
@@ -1,17 +1,17 @@
 import TeamCard, { type Team } from './TeamCard'
 
 type TeamListProps = {
-  teams: Team[]
+    teams: Team[]
 }
 
 function TeamList({ teams }: TeamListProps) {
-  return (
-    <section className="team-list" aria-label="NCAA Basketball teams">
-      {teams.map((team) => (
-        <TeamCard key={team.tid} team={team} />
-      ))}
-    </section>
-  )
+    return (
+        <section className="team-list" aria-label="NCAA Basketball teams">
+            {teams.map((team) => (
+            <TeamCard key={team.tid} team={team} />
+            ))}
+        </section>
+    )
 }
 
 export default TeamList
